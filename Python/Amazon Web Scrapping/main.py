12b7df9125d4b1c9f8feb658f67fa565da1b46c5
David Nii Armah <davidarmah991@gmail.com>
2024-07-24 12:17:50 +0100

    Added Python Projects

---
+++ Python/Amazon Web Scrapping/main.py
@@ -0,0 +1,65 @@
+# Import Libraries
+from bs4 import BeautifulSoup
+import requests
+import time 
+import datetime
+import re
+import smtplib
+
+
+def check_detail():
+    URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id="productTitle").get_text()
+    ratings = soup2.find(id="averageCustomerReviews").get_text()
+    facts  =  soup2.find(id="productFactsDesktop_feature_div").get_text() 
+
+     
+
+    today = datetime.date.today()
+    
+
+    header = ['Title', 'Ratings', 'Facts', 'Date']
+    data = [title, ratings, facts,  today]
+
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
+
+while(True):
+    check_detail()
+    time.sleep(5)
+
+
+
+# If you want to try sending yourself an email (just for fun) when a price hits below a certain level you can try it
+# out with this script
+
+def send_mail():
+    server = smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.ehlo()
+    #server.starttls()
+    server.ehlo()
+    server.login('davidarmah991@gmail.com','xxxxxxxxxxxxxx')
+    
+    subject = "The Shirt you want is below $15! Now is your chance to buy!"
+    body = "David, This is the moment we have been waiting for. Now is your chance to pick up the shirt of your dreams. Don't mess it up! Link here: https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data+analyst+tshirt&qid=1626655184&sr=8-3"
+   
+    msg = f"Subject: {subject}\n\n{body}"
+    
+    server.sendmail(
+        'davidarmah991@gmail.com',
+        msg
+     
+    )
